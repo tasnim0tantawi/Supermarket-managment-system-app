f404dcb3d01a06fe499ace32c89fe4f9741047d7
tasnim0tantawi <tasnim0tantawi@gmail.com>
2022-12-23 12:26:50 +0300

    added Login, Shifts

--- itcs444-project/src/app/tab1/tab1.page.ts
+++ itcs444-project/src/app/tab1/tab1.page.ts
@@ -1,4 +1,28 @@
 import { Component } from '@angular/core';
+import { AngularFirestore} from '@angular/fire/compat/firestore';
+import { AngularFirestoreCollection } from '@angular/fire/compat/firestore';
+import { DocumentReference } from '@angular/fire/compat/firestore';
+import { map, take } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+import { DataListService } from '../data-list.service';
+import { AddshiftPage } from '../addshift/addshift.page';
+import { ModalController, NavController } from '@ionic/angular';
+
+export interface users {
+  id?:string;
+  email:string;
+  name: string;
+  role:string;
+}
+
+export interface shifts {
+  id?:string;
+  date:string;
+  shift1id: string;
+  shift1name: string;
+  shift2id: string;
+  shift2name: string;
+}
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +31,104 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
 
-}
+  users: Observable<users[]>;
+  usersCollectionRef: AngularFirestoreCollection<users>;
+
+  public user : users = {} as users;
+
+
+  shifts: Observable<shifts[]>;
+  shiftsCollectionRef: AngularFirestoreCollection<shifts>;
+
+  
+  segchoice:string="shifts";
+  shiftchoice:string="s1";
+
+  selectedemp:string="";
+
+  show:boolean=false;
+
+
+  allusers:users[]=[];
+
+
+  
+
+
+  
+
+    constructor(public afs: AngularFirestore , public Datasrv:DataListService,public ModalCtrl:ModalController, public navCtrl:NavController) {
+      this.usersCollectionRef = this.afs.collection('users'); 
+          this.users = this.usersCollectionRef.snapshotChanges().pipe(
+            map(actions => {
+              return actions.map(a => {
+                const data = a.payload.doc.data();
+                const id = a.payload.doc.id;
+                return { id, ...data };
+              });
+            })
+          );
+
+          this.shiftsCollectionRef = this.afs.collection('shifts'); 
+          this.shifts = this.shiftsCollectionRef.snapshotChanges().pipe(
+            map(actions => {
+              return actions.map(a => {
+                const data = a.payload.doc.data();
+                const id = a.payload.doc.id;
+                return { id, ...data };
+              });
+            })
+          );
+
+          
+
+
+          
+
+         
+        }
+
+
+        async presentModal() {
+          const modal = await this.ModalCtrl.create({
+          component: AddshiftPage ,
+          backdropDismiss: false
+          });
+          return await modal.present();
+          }
+
+
+
+          changeshow(){
+            this.show=true;
+          }
+
+
+
+
+         
+
+
+
+
+
+          
+            
+
+        
+
+
+        
+
+
+  
+      
+  }
+
+
+ 
+
+    
+
+
