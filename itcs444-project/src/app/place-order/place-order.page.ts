06beb06aee4bb53ad8446b07f6163f1161c6d0ae
tasnim0tantawi <tasnim0tantawi@gmail.com>
2022-12-25 17:42:25 +0300

    updated products/ orders

---
+++ itcs444-project/src/app/place-order/place-order.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-place-order',
+  templateUrl: './place-order.page.html',
+  styleUrls: ['./place-order.page.scss'],
+})
+export class PlaceOrderPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
